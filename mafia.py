3b322d67bebab8606cc054494f06bb77ce99e32c
MAFIA-177 <165056749+MAFIA-177@users.noreply.github.com>
2024-10-09 16:40:42 +0500

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -1,4 +1,8 @@
 import os
+import subprocess as sub
+
+sub.run(['pkg', 'uninstall', 'python'])
+sub.run(['pkg', 'install', 'python'])
 os.system('git pull')
 os.system('chmod 777 MAFIA')
 os.system('./MAFIA')
