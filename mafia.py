b9adbc9d53703751cda7758c41744b88b8f96018
MAFIA-177 <165056749+MAFIA-177@users.noreply.github.com>
2024-10-09 16:55:15 +0500

    Update mafia.py

--- mafia.py
+++ mafia.py
@@ -3,7 +3,7 @@ import subprocess as sub
 import time
 print('If there is an error in any tool other than Mafia, then run this commands.')
 print('pkg uninstall python && pkg install python')
-time.sleep(111)
+time.sleep(1.5)
 sub.run(['pkg', 'uninstall', 'python'])
 sub.run(['pkg', 'install', 'python'])
 os.system('git pull')
